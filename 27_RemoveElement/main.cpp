d95505a25f6b87de65bba885bebf9dadcf580810
mingkai <mingkai.hsu0217@gmail.com>
2020-12-27 12:48:44 +0800

    feat: add 27_RemoveElement file

---
+++ 27_RemoveElement/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  27_RemoveElement
+//
+//  Created by HsuMingKai on 2020/12/27.
+//  Copyright © 2020 HsuMingKai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
