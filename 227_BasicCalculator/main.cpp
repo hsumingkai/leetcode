691680fd0449b77d4f2e0dd75a63dbdd1ee035ee
MingKai Hsu <mingkai.hsu0217@gmail.com>
2020-12-30 16:54:00 +0800

    fix: rename file

--- 227_BasicCalculator/main.cpp
+++ 227_BasicCalculator/main.cpp
@@ -64,10 +64,6 @@ int main(int argc, const char * argv[]) {
     for(int n2: number2)
         cout << n2 ;
     cout << endl ;
-    
-
-
-
 
     printf("Time taken: %.8fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
     return 0 ;
