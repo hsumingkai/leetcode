14bd5b9fdc0dbd131974c26ac162360dc179c546
MingKai Hsu <mingkai.hsu0217@gmail.com>
2021-01-07 07:39:34 +0800

    feat: add orderMap solution for No.169 Problem.

--- 169_MajorityElement/main.cpp
+++ 169_MajorityElement/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector> 
 #include <time.h>
+#include <unordered_map>
 
 using namespace std ;
 class Solution {
 public:
+    // time O(n), space O(1)
     int majorityElement_BoyerMooreVoting(vector<int>& nums) 
     {
         int candidate, sum = 0 ; 
@@ -24,19 +26,36 @@ public:
         }
         return candidate ;
     }
+
+    int majorityElement_unorderMap(vector<int>& nums) 
+    {
+        unordered_map<int, int> counter;
+        for (int num : nums) {
+            if (++counter[num] > nums.size() / 2) {
+                return num;
+            }
+        }
+        return 0;
+    }
+
 };
 int main(int argc, const char * argv[])
 {
     Solution *s = new Solution ;
-    // vector nums = {7, 7, 5, 7, 5, 1 , 5, 7 , 5, 5, 7, 7 , 7, 7, 7, 7} ;
-    vector nums = {1} ;
-    int rs = s->majorityElement_BoyerMooreVoting(nums) ;
-
-    cout << "the majority number : " << rs << endl;
-
     clock_t tStart = clock();
-
+    vector <int> nums = {7, 7, 5, 7, 5, 1 , 5, 7 , 5, 5, 7, 7 , 7, 7, 7, 7} ;
+    int rs = s->majorityElement_BoyerMooreVoting(nums) ;
+    cout << "the majority number : " << rs << endl;
     printf("Time taken: %.8fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+
+    clock_t tStart2 = clock();
+    vector <int> nums2 = {7, 7, 5, 7, 5, 1 , 5, 7 , 5, 5, 7, 7 , 7, 7, 7, 7} ;
+    int rs2 = s->majorityElement_unorderMap(nums2) ;
+    cout << "the majority number : " << rs2 << endl;
+    printf("Time taken: %.8fs\n", (double)(clock() - tStart2)/CLOCKS_PER_SEC);
+
+
+
 
     return 0 ;
 }
