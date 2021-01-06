b13a661e5c0bda9b8b6390fee02240a1beedd2a6
mingkai <mingkai.hsu0217@gmail.com>
2021-01-06 23:08:28 +0800

    feat: add Boyer Moore Voting Solution for No.169 Problem

--- 169_MajorityElement/main.cpp
+++ 169_MajorityElement/main.cpp
@@ -5,16 +5,32 @@
 using namespace std ;
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) 
+    int majorityElement_BoyerMooreVoting(vector<int>& nums) 
     {
-        
+        int candidate, sum = 0 ; 
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(sum == 0)
+                candidate = nums[i] ;
+
+            if(nums[i] == candidate)
+            {
+                ++sum ;
+            }
+            else
+            {
+                --sum ;
+            }
+        }
+        return candidate ;
     }
 };
 int main(int argc, const char * argv[])
 {
     Solution *s = new Solution ;
-    vector nums = {2,3,4,4,4,2,2,2,2} ;
-    int rs = s->majorityElement(nums) ;
+    // vector nums = {7, 7, 5, 7, 5, 1 , 5, 7 , 5, 5, 7, 7 , 7, 7, 7, 7} ;
+    vector nums = {1} ;
+    int rs = s->majorityElement_BoyerMooreVoting(nums) ;
 
     cout << "the majority number : " << rs << endl;
 
