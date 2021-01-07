ecf87ea25c97a999ab56dd25f3407289ffc105bc
MingKai Hsu <mingkai.hsu0217@gmail.com>
2021-01-07 08:39:45 +0800

    feat: add Randomization, BST solution for No.169 Problem

--- 169_MajorityElement/main.cpp
+++ 169_MajorityElement/main.cpp
@@ -2,6 +2,7 @@
 #include <vector> 
 #include <time.h>
 #include <unordered_map>
+#include <map>
 
 using namespace std ;
 class Solution {
@@ -27,6 +28,7 @@ public:
         return candidate ;
     }
 
+    // time O(n), space O(n)
     int majorityElement_unorderMap(vector<int>& nums) 
     {
         unordered_map<int, int> counter;
@@ -38,21 +40,58 @@ public:
         return 0;
     }
 
+    // time O(n), space O(n)
+    int majorityElement_Random(vector<int>& nums) 
+    {
+        bool flag = 1;
+        int n = nums.size();
+        while(1){
+            int x = rand()%n;
+            int val = nums[x], cnt = 0;
+            for(int i=0;i<n;i++){
+                if(nums[i]==val) cnt++;
+            }
+            if(cnt>n/2) return val;
+        }
+        return nums[0];
+    }
+
+    // time O(nlogk), space O(n)
+    int majorityElement_BST(vector<int>& nums) {
+        map<int, int> count;
+        const int n = nums.size();
+        for (const int num : nums)
+            if (++count[num] > n / 2) return num;
+        return -1;
+    }
+
 };
 int main(int argc, const char * argv[])
 {
     Solution *s = new Solution ;
-    clock_t tStart = clock();
     vector <int> nums = {7, 7, 5, 7, 5, 1 , 5, 7 , 5, 5, 7, 7 , 7, 7, 7, 7} ;
+
+    clock_t tStart = clock();
     int rs = s->majorityElement_BoyerMooreVoting(nums) ;
     cout << "the majority number : " << rs << endl;
     printf("Time taken: %.8fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 
     clock_t tStart2 = clock();
-    vector <int> nums2 = {7, 7, 5, 7, 5, 1 , 5, 7 , 5, 5, 7, 7 , 7, 7, 7, 7} ;
-    int rs2 = s->majorityElement_unorderMap(nums2) ;
+    int rs2 = s->majorityElement_unorderMap(nums) ;
     cout << "the majority number : " << rs2 << endl;
     printf("Time taken: %.8fs\n", (double)(clock() - tStart2)/CLOCKS_PER_SEC);
+
+    clock_t tStart3 = clock();
+    int rs3 = s->majorityElement_Random(nums) ;
+    cout << "the majority number : " << rs3 << endl;
+    printf("Time taken: %.8fs\n", (double)(clock() - tStart3)/CLOCKS_PER_SEC);
+
+    clock_t tStart4 = clock();
+    int rs4 = s->majorityElement_BST(nums) ;
+    cout << "the majority number : " << rs4 << endl;
+    printf("Time taken: %.8fs\n", (double)(clock() - tStart4)/CLOCKS_PER_SEC);
+    
+
 
 
 
