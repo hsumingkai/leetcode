0a416bed164d1cc05ca1221390e64dc694b8a12a
mingkai <mingkai.hsu0217@gmail.com>
2021-01-12 23:54:23 +0800

    docs: modify some output

--- 219_ContainsDuplicate2/main.cpp
+++ 219_ContainsDuplicate2/main.cpp
@@ -11,18 +11,23 @@ public:
     bool containsDuplicate_unorderedset(vector<int>& nums, int k){
        unordered_set<int> s;
        
-       if (k <= 0) return false;
-       if (k >= nums.size()) k = nums.size() - 1;
+        if (k <= 0) return false;
+        if (k >= nums.size()) k = nums.size() - 1;
        
-       for (int i = 0; i < nums.size(); i++)
-       {
-           if (i > k) s.erase(nums[i - k - 1]);
-           if (s.find(nums[i]) != s.end()) return true;
-           s.insert(nums[i]);
-       }
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (i > k) s.erase(nums[i - k - 1]);
+            if (s.find(nums[i]) != s.end()) 
+            {
+                return true;
+
+            }
+            s.insert(nums[i]);
+        }
        
-       return false;
+        return false;
     }
+
 
 };
 int main(int argc, const char * argv[])
@@ -31,12 +36,12 @@ int main(int argc, const char * argv[])
     clock_t tStart = clock();
     vector<int> nums = {1,2,3,1,2,3} ;
     // bool rs = s->containsDuplicate_unorderedmap(nums) ;
-    bool rs2 = s->containsDuplicate_unorderedset(nums, 3) ;
+    bool rs2 = s->containsDuplicate_unorderedset(nums, 2) ;
 
     if(rs2)
-        cout << "there is duplicate element in the array . "   << endl ;
+        cout << "rs is true . "   << endl ;
     else
-        cout << "the array is distinct. "   << endl ;
+        cout << "rs is false. "   << endl ;
 
     printf("Time taken: %.8fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 
