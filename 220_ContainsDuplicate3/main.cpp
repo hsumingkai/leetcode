4dc1818e4c0b725dfe9fa2077e0fb7f64206dffb
MingKai Hsu <mingkai.hsu0217@gmail.com>
2021-01-14 08:21:14 +0800

    fix: solve overflow problem by using long type

--- 220_ContainsDuplicate3/main.cpp
+++ 220_ContainsDuplicate3/main.cpp
@@ -10,28 +10,28 @@ class Solution {
 public:
     // ref: https://leetcode.com/problems/contains-duplicate-iii/discuss/61641/C%2B%2B-using-set-(less-10-lines)-with-simple-explanation.
     bool containsNearbyAlmostDuplicate_unorderedset(vector<int>& nums, int k, int t) {
-    set<int> window; // set is ordered automatically 
-    for (int i = 0; i < nums.size(); i++) {
-        if (i > k) window.erase(nums[i-k-1]); // keep the set contains nums i j at most k
-        // |x - nums[i]| <= t  ==> -t <= x - nums[i] <= t;
-        auto pos = window.lower_bound(nums[i] - t); // x-nums[i] >= -t ==> x >= nums[i]-t 
-        // x - nums[i] <= t ==> |x - nums[i]| <= t    
-        if (pos != window.end() && *pos - nums[i] <= t) return true;
-        window.insert(nums[i]);
+        set<long> window; // set is ordered automatically 
+        for (int i = 0; i < nums.size(); i++) {
+            // keep the set contains nums i j at most k
+            long n = nums[i] ;
+            if (i > k) window.erase(nums[i-k-1]); 
+            // |x - nums[i]| <= t  ==> -t <= x - nums[i] <= t;
+            // x-nums[i] >= -t ==> x >= nums[i]-t 
+            auto pos = window.lower_bound(n - t); 
+            // x - nums[i] <= t ==> |x - nums[i]| <= t    
+            if (pos != window.end() && *pos - n <= t) return true;
+            window.insert(n);
+        }
+        return false;
     }
-    return false;
-}
-
-
-
 };
 int main(int argc, const char * argv[])
 {
     Solution *s = new Solution ;
     clock_t tStart = clock();
-    vector<int> nums = {1,5,9,1,5,9} ;
-    // bool rs = s->containsDuplicate_unorderedmap(nums) ;
-    bool rs2 = s->containsNearbyAlmostDuplicate_unorderedset(nums, 2, 3) ;
+    vector<int> nums = {1,5,9} ;
+
+    bool rs2 = s->containsNearbyAlmostDuplicate_unorderedset(nums, 3, 3) ;
 
     if(rs2)
         cout << "rs is true . "   << endl ;
