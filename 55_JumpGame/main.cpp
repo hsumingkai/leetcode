e462d4280a5887a2bc99f6ab73ae7d04be038f11
MingKai Hsu <mingkai.hsu0217@gmail.com>
2021-01-18 13:54:56 +0800

    feat: add best solution for No.55 Problem

--- 55_JumpGame/main.cpp
+++ 55_JumpGame/main.cpp
@@ -4,19 +4,40 @@
 #include <set>
 #include <unordered_set>
 #include <unordered_map>
+#include <algorithm> 
+ 
 
 using namespace std ;
 
 class Solution {
 public:
-    bool canJump(vector<int>& nums) {
-        
+    bool canJump(vector<int>& nums){
+        int maxstep = 0;
+        for (int i = 0; i < nums.size() ; ++i)
+        {
+            if(i > maxstep) 
+                return false ;
+            maxstep= max(nums[i] + i, maxstep) ;
+            
+        }
+
+        return true ;
     }
 };
 
 int main(int argc, const char * argv[])
 {
     Solution *s = new Solution ;
+    vector<int> nums = {3,2,1,0,4} ;
+    bool rs = s->canJump(nums)  ;
+    if(rs)
+    {
+        cout << "can jump to end" ;
+    }
+    else
+    {
+        cout << "cant jump to end" ;
+    }
     
 
     return 0 ;
