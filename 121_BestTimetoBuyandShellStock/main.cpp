095238f1096169e790caa8c9e19984f5c69a3bb1
MingKai Hsu <mingkai.hsu0217@gmail.com>
2021-01-26 08:18:48 +0800

    [feat] add bruteforce and kadane solution for No.121 Problem

--- 121_BestTimetoBuyandShellStock/main.cpp
+++ 121_BestTimetoBuyandShellStock/main.cpp
@@ -10,9 +10,45 @@ using namespace std ;
 // https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        
+    // time:O(n^2) space: O(1)
+    int maxProfit_bruteforce(vector<int>& prices) {
+        if(prices.size() <= 1)
+            return 0 ;
+
+        int profit = 0 ;
+        for(int i = 0; i < prices.size() ; i ++)
+        {
+            for(int j = i +1; j < prices.size() ; j ++)
+            {
+                if(prices[j] - prices[i] > profit)
+                    profit = prices[j] - prices[i] ;
+            }
+        }
+        return profit ;
     }
+    // time: O(n), space: O(1)
+    // ref: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/solution/
+    int maxProfit(vector<int>& prices) {
+        int minprice = INT_MAX;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.size(); i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
+    }
+    // ref: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/discuss/39038/Kadane's-Algorithm-Since-no-one-has-mentioned-about-this-so-far-%3A)-(In-case-if-interviewer-twists-the-input)
+    int maxProfit_Kadane(vector<int>& prices) {
+        int maxCur = 0, maxSoFar = 0;
+        for(int i = 1; i < prices.size(); i++) {
+            maxCur = max(0, maxCur += prices[i] - prices[i-1]);
+            maxSoFar = max(maxCur, maxSoFar);
+        }
+        return maxSoFar;
+    }
+
 };
 
 int main(int argc, const char * argv[])
@@ -20,7 +56,11 @@ int main(int argc, const char * argv[])
     clock_t tStart = clock();
 
     Solution *s = new Solution ;
-    vector<int> nums = {7,1,5,3,6,4} ;
+    // vector<int> nums = {7,1,5,3,6,4} ;
+    vector<int> nums = {0,1} ;
+    int rs = s->maxProfit_Kadane(nums) ;
+
+    cout << "the max profit is " << rs << endl; 
 
 
     printf("Time taken: %.8fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
