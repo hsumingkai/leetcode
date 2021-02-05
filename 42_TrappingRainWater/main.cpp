291d0e9ab0ea12f28fce945c29ca14ecb84546e1
MingKai Hsu <mingkai.hsu0217@gmail.com>
2021-02-05 08:17:50 +0800

    feat: add dp solution for No.42 problem

--- 42_TrappingRainWater/main.cpp
+++ 42_TrappingRainWater/main.cpp
@@ -4,14 +4,31 @@
 #include <set>
 #include <unordered_set>
 #include <unordered_map>
+#include <stack>
 #include <algorithm> 
 
 using namespace std ;
 // https://leetcode.com/problems/trapping-rain-water/
 class Solution {
 public:
-    int maxArea(vector<int>& height) {
-        
+    int trap_dp(vector<int>& height) {
+        if(height.size() == 0 )
+            return 0;
+        int ans = 0;
+        int size = height.size();
+        vector<int> left_max(size), right_max(size);
+        left_max[0] = height[0];
+        for (int i = 1; i < size; i++) {
+            left_max[i] = max(height[i], left_max[i - 1]);
+        }
+        right_max[size - 1] = height[size - 1];
+        for (int i = size - 2; i >= 0; i--) {
+            right_max[i] = max(height[i], right_max[i + 1]);
+        }
+        for (int i = 1; i < size - 1; i++) {
+            ans += min(left_max[i], right_max[i]) - height[i];
+        }
+        return ans;
     }
 
 };
@@ -21,9 +38,10 @@ int main(int argc, const char * argv[])
     clock_t tStart = clock();
 
     Solution *s = new Solution ;
-    vector<int> nums = {1,8,6,2,5,4,8,3,7} ;
+    vector<int> nums = {0,1,0,2,1,0,1,3,2,1,2,1} ;
+    int rs = s->trap_dp(nums) ;
 
-
+    cout << "the rs is " << rs << endl ;
     printf("Time taken: %.8fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 
     return 0 ;
